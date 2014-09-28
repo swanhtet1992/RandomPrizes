5c0638ebdbf899ed912d767ec089ea11d2133885
Ye Lin Aung <me@yelinaung.com>
2014-09-28 15:27:20 +0630

    remove unused menu

--- app/src/main/java/yelinaung/randomprizes/MyActivity.java
+++ app/src/main/java/yelinaung/randomprizes/MyActivity.java
@@ -7,8 +7,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
@@ -112,27 +110,5 @@ public class MyActivity extends Activity {
       dialog.show();
       swipeRefreshLayout.setRefreshing(false);
     }
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.my, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
   }
 }
